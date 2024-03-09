eda81216bfab549d9220385e3e201662589a970e
tushar007ff <155503833+tushar007ff@users.noreply.github.com>
2024-03-09 17:53:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/akshayxt/Akshat",
+    "https://github.com/tushar007ff/tushar007ff",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(
